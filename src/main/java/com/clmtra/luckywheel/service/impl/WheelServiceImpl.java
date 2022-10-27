a8e6abae028a06d017b1aa0edb0f6a545e2f8219
Dmitrii Smirnov <dmitrii.smirnov@abfprogramming.com>
2022-10-27 05:20:43 +0400

    data init

--- src/main/java/com/clmtra/luckywheel/service/impl/WheelServiceImpl.java
+++ src/main/java/com/clmtra/luckywheel/service/impl/WheelServiceImpl.java
@@ -4,6 +4,7 @@ import com.clmtra.luckywheel.service.WheelService;
 import com.clmtra.luckywheel.transfer.OrderDTO;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -11,6 +12,11 @@ import java.util.concurrent.ConcurrentHashMap;
 public class WheelServiceImpl implements WheelService {
 
     private final static Map<String, String> wheelStorage = new ConcurrentHashMap<>();
+
+    @PostConstruct
+    public void initStorage() {
+        wheelStorage.put("0", "Hello");
+    }
 
     @Override
     public void addWheelToStorage(OrderDTO orderDTO) {
